703bdc7bee016d09859b05533a7650567b0de995
architmehrotra <architmehrotra70@gmail.com>
2016-09-16 20:40:48 +0530

    Final edition

--- Q2.scala
+++ Q2.scala
@@ -4,7 +4,7 @@ object Q2{
 		lst match{
 			case Nil => (Nil, Nil)
 			case hd::tail if lst.length==1 => (List(hd), Nil) 
-			case hd::tail if lst.length>1 => (hd::Split(tail.init)._1, Split(tail.init)._2:::List(lst.last))
+			case hd::tail if lst.length>1 => (hd::Split(tail.init)._1, Split(tail.init)._1:::List(lst.last))
 		}
 		}
 		Console.println("Splitting (1,2,3,4)" + Split(List(1,2,3,4)))
--- Q6.scala
+++ Q6.scala
@@ -1,22 +1,21 @@
-object Q6
-{
-	def pivotSort(inputlist:List[Int], inputnum:Int):List[Int]=
-	{
+object Q6{
+
+def main(args: Array[String]): Unit= {
+
+	def pivotSort(inputlist:List[Int], inputnum:Int):List[Int]= {
 		inputlist match {
 			case head::rest if (head <= inputnum) => head::pivotSort(rest,inputnum)
 			case Nil => List(inputnum)
 			case head::rest if (head > inputnum) => pivotSort(rest,inputnum) ::: List(head)
 		}
 	}
-	def quickSort(inputlist:List[Int]):List[Int]=
-	{
+	def quickSort(inputlist:List[Int]):List[Int]= {
+	
 		inputlist match {
-			case hd::rest => pivotSort(inputlist,hd) 
 			case Nil => Nil
+			case hd::rest => pivotSort(inputlist,hd) 
 		}
 	}	
-def main(args: Array[String]): Unit= 
-	{
 		println("Unsorted list is (17, 1, 24, 35, 16, 29); sorted list is:" + quickSort(List(17, 1, 24, 35, 16, 29)))
 	}
 }
